8e3fac07800b4e003fbb10386516b2d3b10f8c1f
German Fica <germafica@gmail.com>
2023-11-28 17:18:33 -0300

    Update code

--- Project1Cpp/main.c
+++ Project1Cpp/main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "player.h"
 #include "mob.h"
 
 int main() {
+    int num = 15;
+    int* numPtr = &num;
     Player player = { 100 };  // Create a new player.
-    Mob mob = { 100 };        // Create a new mob.
+    //Mob mobInstance = { 100 };  // Create a new mob directly, not a pointer.
+    Mob* mob1 = malloc(sizeof(Mob));  // Allocate memory dynamically for mob1
+    if (mob1 == NULL) {
+        perror("Error asignando memoria para mob1");
+        exit(1);
+    }
+    mob1->health = 200;
 
-    dealDamage(&player, &mob);  // Player deals damage to mob.
-    takeDamage_Mob(&mob, 30);  // Mob takes damage.
+    Mob* mob2 = mob1;  // Just point mob2 to the same memory as mob1
+    mob2->health -= 90;
+
+    //Mob* mob3 = { 500 };
+    //mob3->health = 100;
+
+    printf("Mob1 health %d\n", mob1->health);
+    printf("Mob2 health %d\n", mob2->health);
+
+
+    //dealDamage(&player, &mobInstance);  // Passing address of the mob instance
+    //takeDamage_Mob(&mobInstance, 30);
+
+    free(mob1);
+    // Do NOT free mob2 because it points to the same memory as mob1
 
     return 0;
 }
--- Project1Cpp/mob.c
+++ Project1Cpp/mob.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 void takeDamage_Mob(Mob* mob, int damage) {
+    printf("Address of mob in takeDamage_Mob: %p\n", (void*)&mob);
     mob->health -= damage;
     if (mob->health < 0) mob->health = 0;
     printf("Mob now has %d health.\n", mob->health);
